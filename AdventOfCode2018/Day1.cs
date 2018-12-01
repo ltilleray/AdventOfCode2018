48f402e0f84d90ff3fcea069a43445ce134e571e
Luke <ltilleray@gmail.com>
2018-12-01 13:14:52 +0000

    Unit tests for Day 1 Part 1

--- AdventOfCode2018/Day1.cs
+++ AdventOfCode2018/Day1.cs
@@ -5,19 +5,19 @@ using System.Text;
 
 namespace AdventOfCode2018
 {
-    class Day1
+    public class Day1
     {
 
         public int SolvePart1()
         {
             var result = 0;
             foreach (string line in ReadFile()) {
-                result = addNumbers(result, Int32.Parse(line));
+                result = AddNumbers(result, Int32.Parse(line));
             }
             return result;
         }
 
-        public int addNumbers(int firstNumber, int secondNumber)
+        public int AddNumbers(int firstNumber, int secondNumber)
         {
             return firstNumber + secondNumber;
         }
---
+++ AdventOfCode2018Tests/Day1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using AdventOfCode2018;
+
+namespace AdventOfCode2018Tests
+{
+    public class Day1Tests
+    {
+        [Fact]
+        public void AddNumbersTest()
+        {
+            Assert.Equal(3, new Day1().AddNumbers(-4, 7));
+        }
+
+        [Fact]
+        public void SolvePart1Test()
+        {
+            Assert.Equal(477, new Day1().SolvePart1());
+        }
+    }
+}
